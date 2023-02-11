6bc2a6302aa9842754d5544ec530b130dbaad427
yeonho jeon <wjsdusghek@naver.com>
2023-02-11 15:15:48 +0900

    Level2 중간정리

---
+++ GameEngineCore/Button.cpp
@@ -0,0 +1,82 @@
+#include "Button.h"
+#include <GameEnginePlatform/GameEngineInput.h>
+
+Button::Button()
+{
+	State = ButtonState::Release;
+}
+
+Button::~Button()
+{
+}
+
+
+void Button::SetTargetCollisionGroup(int _PointTargetGroup)
+{
+	PointTargetGroup = _PointTargetGroup;
+	if (nullptr == ButtonCollision)
+	{
+		ButtonCollision = CreateCollision(PointTargetGroup);
+	}
+}
+
+
+void Button::Start()
+{
+	ButtonRender = CreateRender();
+	ButtonCollision = CreateCollision();
+}
+
+void Button::SetRenderOrder(int _Value)
+{
+	ButtonRender->SetOrder(_Value);
+}
+
+void Button::SetScale(float4 _Scale)
+{
+	Scale = _Scale;
+	ButtonCollision->SetScale(Scale);
+	// Render->SetScale(_Scale);
+}
+
+void Button::Update(float _DeltaTime)
+{
+	State = ButtonState::Release;
+
+	if (true == ButtonCollision->Collision({ .TargetGroup = PointTargetGroup, .TargetColType = CollisionType::CT_Point, .ThisColType = ButtonCollisionType }))
+	{
+		if (true == GameEngineInput::IsUp("EngineMouseLeft") && nullptr != ClickPtr)
+		{
+			ClickPtr();
+		}
+		else if (true == GameEngineInput::IsFree("EngineMouseLeft"))
+		{
+			State = ButtonState::Hover;
+		}
+		else if (true == GameEngineInput::IsPress("EngineMouseLeft"))
+		{
+			State = ButtonState::Press;
+		}
+	}
+
+	switch (State)
+	{
+	case ButtonState::Release:
+		CurImageName = ReleaseImageName;
+		ButtonRender->SetImage(ReleaseImageName);
+		ButtonRender->SetScale(Scale);
+		break;
+	case ButtonState::Press:
+		CurImageName = PressImageName;
+		ButtonRender->SetImage(PressImageName);
+		ButtonRender->SetScale(Scale);
+		break;
+	case ButtonState::Hover:
+		CurImageName = HoverImageName;
+		ButtonRender->SetImage(HoverImageName);
+		ButtonRender->SetScale(Scale);
+		break;
+	default:
+		break;
+	}
+}
---
+++ GameEngineCore/Button.h
@@ -0,0 +1,86 @@
+#pragma once
+#include "GameEngineActor.h"
+#include "GameEngineCollision.h"
+#include "GameEngineRender.h"
+
+enum class ButtonState
+{
+	Release, // 안눌렸다.
+	Press, // 눌렸다.
+	Hover, // 나를 누를수 있는 녀석이 위에서 맴돌고 있다.
+};
+
+// 설명 :
+class GameEngineRender;
+class Button : public GameEngineActor
+{
+public:
+	// constrcuter destructer
+	Button();
+	~Button();
+
+	// delete Function
+	Button(const Button& _Other) = delete;
+	Button(Button&& _Other) noexcept = delete;
+	Button& operator=(const Button& _Other) = delete;
+	Button& operator=(Button&& _Other) noexcept = delete;
+
+	void SetRenderOrder(int _Value);
+
+	void SetScale(float4 _Scale);
+
+	void SetClickCallBack(void(*_ClickPtr)())
+	{
+		ClickPtr = _ClickPtr;
+	}
+
+	void SetTargetCollisionGroup(int _PointTargetGroup);
+
+	void SetButtonCollisionType(CollisionType _ButtonCollisionType)
+	{
+		ButtonCollisionType = _ButtonCollisionType;
+	}
+
+	void SetHoverImage(const std::string_view& _Name)
+	{
+		HoverImageName = _Name;
+	}
+	void SetReleaseImage(const std::string_view& _Name)
+	{
+		ReleaseImageName = _Name;
+	}
+	void SetPressImage(const std::string_view& _Name)
+	{
+		PressImageName = _Name;
+	}
+
+	GameEngineRender* GetButtonRender()
+	{
+		return ButtonRender;
+	}
+
+	GameEngineCollision* GetButtonCollision()
+	{
+		return ButtonCollision;
+	}
+
+protected:
+	void Start() override;
+	void Update(float _DeltaTime) override;
+
+private:
+	GameEngineRender* ButtonRender = nullptr;
+	GameEngineCollision* ButtonCollision = nullptr;
+	int PointTargetGroup = 0;
+	CollisionType ButtonCollisionType = CollisionType::CT_Rect;
+	void(*ClickPtr)() = nullptr;
+
+	float4 Scale;
+	ButtonState State;
+	std::string CurImageName;
+	std::string HoverImageName;
+	std::string ReleaseImageName;
+	std::string PressImageName;
+
+};
+
